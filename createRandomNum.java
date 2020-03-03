c9e41ccf1127aaf9bcfd25b1b2ea31b5cc2f7f32
Luan Tran <39330110+minhluanlqd@users.noreply.github.com>
2020-03-02 21:57:48 -0500

    Add files via upload

--- createRandomNum.java
+++ createRandomNum.java
@@ -7,8 +7,15 @@ public class createRandomNum{
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(1000000) + 1; // random number between 1 and ...
         }
+        arr[arr.length/4] = (-1)*arr[arr.length/4];
+        arr[arr.length/2] = (-1)*arr[arr.length/2];
+        arr[3*(arr.length/4)] = (-1)*arr[3*(arr.length/4)];
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
+        // System.out.println(arr[arr.length/4]);
+        // System.out.println(arr[arr.length/2]);
+        // System.out.println(arr[3*(arr.length/4)]);
+        
     }
 }
