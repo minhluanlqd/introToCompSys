39c4f8b9a8c11013dc8d47d7af50cade7b59bc4c
LUAN TRAN <lmt185@engsoft.rutgers.edu>
2020-03-07 23:42:05 -0500

    new update prob 2-3

--- problem2_3.c
+++ problem2_3.c
@@ -73,52 +73,121 @@ int main(){
 //	double time_spent = 0.0;
 //	clock_t begin = clock();
 
-	int myPipe[2];
-	int maxPipe[2];
-        int arr[1000];
+	int Pipe1[2];
+	int Pipe2[2];
+	int Pipe3[2];
+	int Pipe4[2];
+	int Pipe5[2];
+	int Pipe6[2];
+        int arr[100000];   // FIX THIS LENGTH ACCORDING TO THE FILE
 	int size = sizeof(arr)/sizeof(arr[0]);
-	char outString[100];
-	char *filename = "./File1k.txt";
-	char *resultFile ="./result_3_1k.txt";
+	int interval = (size - 1)/7;
+	int b1 = interval * 1;
+	int b2 = interval * 2;
+	int b3 = interval * 3;
+	int b4 = interval * 4;
+	int b5 = interval * 5;
+	int b6 = interval * 6;
+	int b7 = size;
+	char outString1[100];
+	char outString2[100];
+	char outString3[100];
+	char outString4[100];
+	char outString5[100];
+	char outString6[100];
+	char *filename = "./File100k.txt"; // FIX NAME FILE
+	char *resultFile ="./result_3_100k.txt"; // FIX NAME OF RESULT FILE
 	read_ints(filename,arr);
+	pipe(Pipe1);
+	pipe(Pipe2);
+	pipe(Pipe3);
+	pipe(Pipe4);
+	pipe(Pipe5);
+	pipe(Pipe6);
 
-//	int createPipe=	pipe(myPipe);
-//	pipe(maxPipe);
-//	if (createPipe == -1){
-//		fprintf(stderr, "Pipe failed");	
-//	}
 	int p = fork();
 	int p1 = fork();
 
 	if(p > 0 && p1 > 0){ // Parent
 		wait(NULL);
-		printf("Parent: Hi I am process %d and my parent is %d\n", getpid(), getppid());
+		wait(NULL);
+		read(Pipe1[0], outString1, 100);
+		read(Pipe2[0], outString2, 100);
+		read(Pipe3[0], outString3, 100);
+		read(Pipe4[0], outString4, 100);
+		read(Pipe5[0], outString5, 100);
+		read(Pipe6[0], outString6, 100);
+		int maxGrChild[7];
+		read(Pipe1[0], &maxGrChild[1], sizeof(maxGrChild[1]));
+		read(Pipe2[0], &maxGrChild[2], sizeof(maxGrChild[2]));
+		read(Pipe3[0], &maxGrChild[3], sizeof(maxGrChild[3]));
+		read(Pipe4[0], &maxGrChild[4], sizeof(maxGrChild[4]));
+		read(Pipe5[0], &maxGrChild[5], sizeof(maxGrChild[5]));
+		read(Pipe6[0], &maxGrChild[6], sizeof(maxGrChild[6]));
+		maxGrChild[0] = find_max(arr,b6,b7); // this elem is max of parent
+		int max = find_max(maxGrChild, 0 , 7);
+		FILE *file = fopen(resultFile, "w");
+		fprintf(file,"Parent: Hi I am process %d and my parent is %d\nMax is: %d\n", getpid(), getppid(),maxGrChild[0]);
+		printf("maxGrChild1 is: %d\nmaxGrChild2 is: %d\nmaxGrChild3 is: %d\n", maxGrChild[1], maxGrChild[2], maxGrChild[3]);
+		printf("maxGrChild4 is: %d\nmaxGrChild5 is: %d\nmaxGrChild6 is: %d\n", maxGrChild[4], maxGrChild[5], maxGrChild[6]);
+		fprintf(file,"%s\n", outString1);
+		fprintf(file,"%s\n", outString2);
+		fprintf(file,"%s\n", outString3);
+		fprintf(file,"%s\n", outString4);
+		fprintf(file,"%s\n", outString5);
+		fprintf(file,"%s\n", outString6);
+		fprintf(file,"Total max is: %d\n", max);
+		fclose(file);
 	}else if (p == 0 && p1 > 0){ // 1st child
-		printf("1st Child: Hi I am process %d and my parent is %d\n", getpid(), getppid());
+		int max = find_max(arr,b4,b5);
+		printf("1st Child: Hi I am process %d and my parent is %d\nMax is: %d\n", getpid(), getppid(),max);
+		sprintf(outString5, "Hi I am  process %d and my parent is %d\nMax is: %d", getpid(),getppid(), max);
+		write(Pipe5[1], outString5, 100);
+		write(Pipe5[1], &max, sizeof(max));
 		int p2 = fork(); // 1st child born 2nd grandchild
 		if(p2 == 0){
-			printf("2nd GrChild: Hi I am process %d and my parent is %d\n", getpid(), getppid());
+			int max = find_max(arr,b1,b2);
+			printf("2nd GrChild: Hi I am process %d and my parent is %d\nMax is: %d\n", getpid(), getppid(),max);
+			sprintf(outString2, "Hi I am process %d and my parent is %d\nMax is: %d\n",getpid(), getppid(), max);
+			write(Pipe2[1], outString2, 100);
+			write(Pipe2[1], &max, sizeof(max));
 		}else{
+			wait(NULL);
 			wait(NULL);
 		}
 	}else if (p > 0 && p1 == 0){ // 2nd child
-		printf("2nd Child: Hi I am process %d and my parent is %d\n", getpid(), getppid());
+		int max = find_max(arr,b5,b6);
+		printf("2nd Child: Hi I am process %d and my parent is %d\nMax is: %d\n", getpid(), getppid(),max);
+		sprintf(outString6, "Hi I am  process %d and my parent is %d\nMax is: %d", getpid(),getppid(), max);
+		write(Pipe6[1], outString6, 100);
+		write(Pipe6[1], &max, sizeof(max));
 		int p3 = fork(); // 2nd child born 1st granchild
 		if(p3 == 0){ // 3rd grandchild
-			printf("3rd GrChild: Hi I am process %d and my parent is %d\n", getpid(), getppid());
-
+			int max = find_max(arr,b2,b3);
+			printf("3rd GrChild: Hi I am process %d and my parent is %d\nMax is: %d\n", getpid(), getppid(),max);
+			sprintf(outString3,"Hi I am process %d and my parent is %d\nMax is: %d\n", getpid(), getppid(), max);
+			write(Pipe3[1], outString3, 100);
+			write(Pipe3[1], &max, sizeof(max));
 		}else{
-//			wait(NULL);
 			int p4 = fork(); //2nd child born 2nd grandchild
+//			wait(NULL);
 			if(p4 == 0){ // 4th grandchild
-				printf("4th GrChild: Hi I am process %d and my parent is %d\n", getpid(), getppid());
-
+				int max = find_max(arr, b3, b4);
+				printf("4th GrChild: Hi I am process %d and my parent is %d\nMax is: %d\n", getpid(), getppid(),max);
+				sprintf(outString4, "Hi I am  process %d and my parent is %d\nMax is: %d", getpid(),getppid(), max);
+				write(Pipe4[1], outString4, 100);
+				write(Pipe4[1], &max, sizeof(max));
 			}else{
+				wait(NULL);
 				wait(NULL);
 			}
 		}
 	}else{ // 1st grandchild
-		printf("1st GrChild: Hi I am process %d and my parent is %d\n", getpid(), getppid());
+		int max = find_max(arr,0,b1);
+		printf("1st GrChild: Hi I am process %d and my parent is %d\nMax is: %d\n", getpid(), getppid(),max);
+		sprintf(outString1,"Hi I am process %d and my parent is %d\nMax is: %d\n", getpid(), getppid(),max);
+		write(Pipe1[1],outString1, 100);
+		write(Pipe1[1],&max, sizeof(max));
 	}
 //	else if (p == 0) { //Child process
 //		int max1 = find_max(arr, 0, size/2-1);
@@ -152,6 +221,5 @@ int main(){
 //		time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
 //		printf("\nTime elpased is %f seconds\n", time_spent);
 //	}
-
         return 0;
 }
